e8cbb00c9abafe6ada52b9edadcbacccc022ff9e
Alonso <luis.alonso.16@hotmail.com>
2020-02-10 00:17:52 -0700

    Update book model

--- application/getAll/main.go
+++ application/getAll/main.go
@@ -15,10 +15,11 @@ import (
 type BookModel struct {
 	ID          string    `json:"book_id"`
 	Name        string    `json:"name"`
+	S3Url		*string	  `json:"s3_url"`
 	Authors     []*string `json:"authors"`
 	Categories  []*string `json:"categories"`
-	PublishedAt int64      `json:"published_at"`
-	CreatedAt   int64      `json:"created_at"`
+	PublishedAt int64     `json:"published_at"`
+	CreatedAt   int64     `json:"created_at"`
 }
 
 // LambdaEvent Lambda Event (params)
